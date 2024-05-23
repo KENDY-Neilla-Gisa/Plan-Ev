eec0f77745c1202dba3fd66d55fed87c3e7dbf0a
kendy112 <kendyneilla@gmail.com>
2024-05-23 21:28:25 +0300

    Some changes were made

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+document.addEventListener('DOMContentLoaded', (event) => {
+    let menu = document.querySelector('#menu-bars');
+    let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+    if (menu && navbar) {
+        menu.addEventListener('click', () => {
+            menu.classList.toggle('fa-times');
+            navbar.classList.toggle('active');
+        });
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+        window.addEventListener('scroll', () => {
+            menu.classList.remove('fa-times');
+            navbar.classList.remove('active');
+        });
+    } else {
+        console.error('Menu or Navbar element not found');
+    }
+});
